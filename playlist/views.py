be0de64259c62bc93875bf78bb852629274fb3c6
GBroderick <galen.broderick@gmail.com>
2019-05-30 22:39:58 -0700

    add requests library, make first proper KEXP API call

--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,15 @@
+import json, requests
+from datetime import datetime, timedelta
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def playlist(request):
+    endtime = datetime.utcnow()
+    starttime = endtime - timedelta(hours=1)
+    starttime_string = starttime.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    resp = requests.get('http://128.208.196.80/play/?begin_time=' + starttime_string + '&ordering=-airdate&limit=40')
+    # data = resp.json()
+    data = resp
+    return HttpResponse(data)
