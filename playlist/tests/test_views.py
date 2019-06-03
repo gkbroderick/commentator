58cf56ac665ddd72a9370ca70b2da43813b25a52
GBroderick <galen.broderick@gmail.com>
2019-06-03 09:39:31 -0700

    further tests for Ajax view

--- playlist/tests/test_views.py
+++ playlist/tests/test_views.py
@@ -20,7 +20,6 @@ class CommentAjaxCreationView(TestCase):
         response = c.post(reverse('comment'), data, **kwargs)
         json_string = response.content
         cls.response_data = json.loads(json_string)
-        print(cls.response_data)
 
     def test_comment_created_response_id_matches_latest_created_id(self):
         response_comment_id = self.response_data['comment_id']
@@ -48,17 +47,51 @@ class CommentAjaxCreationView(TestCase):
         self.assertEquals(response_play_item_kexp_id, latest_comment_play_item_kexp_id)
 
 
+class CommentAjaxUpdateView(TestCase):
+    def setUp(self):
+        self.kwargs = {'HTTP_X_REQUESTED_WITH':'XMLHttpRequest'}
+        self.data_input = {
+            'play_instance_kexp_id': 267953,
+            'play_instance_name': 'Someone Great',
+            'play_instance_airdate': 'June 2, 2019, 6:15 p.m.',
+            'comment_id' : 0,
+            'comment_text' : 'Another Perfect Summer Song',
+        }
+        response = self.client.post(reverse('comment'), self.data_input, **self.kwargs)
+        json_string = response.content
+        self.response_data = json.loads(json_string)
 
+    def test_comment_created_response_id_matches_latest_created_id_again_for_update_tests(self):
+        response_comment_id = self.response_data['comment_id']
+        latest_comment_id = Comment.objects.latest('date_created').id
+        self.assertEquals(response_comment_id, latest_comment_id)
 
-#Comment should not save if text blank
+    def test_comment_update_values_correctly_correspond(self):
+        original_response = self.response_data
+        new_data_input = {
+            'play_instance_kexp_id': 267953,
+            'play_instance_name': 'Someone Great',
+            'play_instance_airdate': 'June 2, 2019, 6:15 p.m.',
+            'comment_id' : original_response['comment_id'],
+            'comment_text' : 'Another Perfect Summer Song, again',
+        }
+        response = self.client.post(reverse('comment'), new_data_input, **self.kwargs)
+        json_string = response.content
+        response_data_update = json.loads(json_string)
 
+        updated_comment = Comment.objects.latest('date_created')
 
+        # make sure ids are the same
+        self.assertEquals(response_data_update['comment_id'], updated_comment.id)
 
-#---comment updating
+        #make sure response text matches db comment text
+        self.assertEquals(response_data_update['comment_text'], updated_comment.comment_text)
 
-# Comment Text should still not be blank
+        #make sure different text has been added
+        self.assertNotEquals(original_response['comment_text'], updated_comment.comment_text)
 
-#Comment Last Edited time should update
-#Comment last_edited time should be more recent than created
+        #make sure original version is using the same kexp play id
+        self.assertEquals(int(original_response['play_instance_kexp_id']), updated_comment.play_instance.kexp_play_id)
 
-#play_instance ForeignKey should be the same
+        #make sure update version is using the same kexp play id
+        self.assertEquals(int(response_data_update['play_instance_kexp_id']), updated_comment.play_instance.kexp_play_id)
